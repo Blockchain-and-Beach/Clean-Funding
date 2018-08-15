ec8f882d412ffd3542db489789cc3c1276f0007b
jodnddus <lockhost23@gmail.com>
2018-08-15 21:21:37 +0900

    nem2-sdk 다운로드 부가적인 세팅

---
+++ Blcokchain_Part/Source.ts
@@ -0,0 +1,4 @@
+//개발에 필요한 기능들을 nem2-sdk에서 import함
+import {
+    Account, Address, Deadline, UInt64, NetworkType, PlainMessage, TransferTransaction, Mosaic, MosaicId, TransactionHttp
+} from 'nem2-sdk';
