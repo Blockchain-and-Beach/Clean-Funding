84529cabd3c82b1aacff833d097a70594192fd16
Packet <199sd@naver.com>
2018-08-16 22:22:58 +0900

    add paymentSchema

--- models/postModel.js
+++ models/postModel.js
@@ -7,6 +7,17 @@ const commentSchema = new mongoose.Schema({
     author: {type: userSchema, required: true},
     content: {type: String, required: true}
 });
+const paymentSchema = new mongoose.Schema({
+    name: {type:String, required: true},
+    amount:{type:Number,required: true},
+    price:{type:Number, require: true},
+    totalPrice:{type:Number, required: true}
+});
+
+const paymentListSchema = new mongoose.Schema({
+    payments:{type:[paymentSchema]},
+    totalPrice: {type: Number, default:calcTotalPrice()}
+});
 
 const postSchema = new mongoose.Schema({
     title:{ type: String, unique: true, required: true, index:true},
@@ -24,6 +35,13 @@ postSchema.statics.findByTitle = async title=>{
     } catch(err){
         console.error(err);
     }
+};
+paymentListSchema.methods.calcTotalPrice = ()=>{
+    let totalPrice =0;
+    this.payments.forEach(element=>{
+        totalPrice += element.price;
+    });
+    return totalPrice;
 };
 
 exports.postSchema = postSchema;
