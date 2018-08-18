f7cc46beaa1dbf37c3d6e9fac208d5f83f5f5641
chin0 <pypi1103@gmail.com>
2018-08-18 10:27:49 +0900

    change namespace interface

--- lib/namespace.ts
+++ lib/namespace.ts
@@ -1,7 +1,13 @@
 import { Account,UInt64,TransactionHttp, RegisterNamespaceTransaction, Deadline, NetworkType } from "nem2-sdk";
 
+
+function defaultErrorHandle(err: any) : void{
+    console.log("Error!!: " + err);
+}
+
 //nsName을 이름으로 하는 네임스페이스를 등록하는 트랜잭션을 요청하는 함수.
-function RegisterRootNamespace(nsName) {
+//nsName: 루트 네임스페이스 이름, successHandle: 성공시 핸들링할 함수, failHandle: 실패시 핸들링할 함수.
+function RegisterRootNamespace(nsName,successHandle, failHandle=defaultErrorHandle) {
     const transactionHttp = new TransactionHttp(process.env.HOST);
     const privateKey = process.env.ADMIN_KEY as string;
     const account = Account.createFromPrivateKey(privateKey,NetworkType.TEST_NET);
@@ -18,12 +24,12 @@ function RegisterRootNamespace(nsName) {
 
     transactionHttp
         .announce(signedTransaction)
-        .subscribe(suc => console.log("success: " + suc), err => console.log("Error!!: " + err));    
+        .subscribe(suc => successHandle("success: " + suc), err => failHandle(err));    
 }
 
 //루트 네임스페이스에 서브도메인생성.
 //rootNsName: 루트네임스페이스, nsName: 네임스페이스  
-function RegisterSubNamespace(rootNsName: string, nsName: string) {
+function RegisterSubNamespace(rootNsName: string, nsName: string,successHandle,failHandle=defaultErrorHandle) {
     const transactionHttp = new TransactionHttp(process.env.HOST);
 
     const privateKey = process.env.ADMIN_KEY;
@@ -40,7 +46,7 @@ function RegisterSubNamespace(rootNsName: string, nsName: string) {
 
     transactionHttp
         .announce(signedTransaction)
-        .subscribe(suc => console.log("success: " + suc), err => console.log("Error!!: " + err));    
+        .subscribe(suc => successHandle(suc), err => failHandle(err));    
 }
 
 export {RegisterRootNamespace,RegisterSubNamespace};
